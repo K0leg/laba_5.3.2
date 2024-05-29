08b939592186bb7bb786c89f08119a78179d6454
K0leg <olegkovtun2005ua@gmail.com>
2024-05-29 18:17:35 +0300

    Add files via upload

---
+++ src/main/java/com/company/third.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class third {
+
+    public static void main(String[] args) {
+
+        for(int i=1; i <= 1000; i++){
+            if((10 % i) == 0){
+                System.out.println(i);
+            }
+        }
+
+    }
+
+}

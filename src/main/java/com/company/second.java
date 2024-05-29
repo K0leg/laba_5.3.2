35f33f9433767016ffc15a2eee1944c7fa3f7dc2
K0leg <olegkovtun2005ua@gmail.com>
2024-05-29 18:14:58 +0300

    Add files via upload

---
+++ src/main/java/com/company/second.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class second {
+
+    public static void main(String[] args) {
+        for(int a=2; a <= 5000; a=a*2-1){
+            System.out.println(a);
+        }
+    }
+
+}
